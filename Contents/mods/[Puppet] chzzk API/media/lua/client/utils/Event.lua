eb8208b97ff508ac7a2bb237fde86dcec7ff0c1f
t3qquq <63445961+t3qquq@users.noreply.github.com>
2026-07-22 08:24:32 +0900

    [Fix] 버프디버프 사운드 수정

--- Contents/mods/[Puppet] chzzk API/media/lua/client/utils/Event.lua
+++ Contents/mods/[Puppet] chzzk API/media/lua/client/utils/Event.lua
@@ -6,7 +6,7 @@ function _a.a(a)
     _c.b("onMoodleEvent FUNCTION START")
     _c.textUpdateTimer = 0
     _c.displayStartTime = 0
-    getSoundManager():PlaySound("slot", false, 1.0)
+    -- getSoundManager():PlaySound("slot", false, 1.0)
     if a then
         Events.OnTick.Add(_b.a)
     else
@@ -17,7 +17,7 @@ end
 
 function _a.b(a)
     _c.b("onZombieEvent FUNCTION START")
-    getSoundManager():PlaySound("slot", false, 1.0)
+    -- getSoundManager():PlaySound("slot", false, 1.0)
     Events.OnTick.Add(_b.c)
     _c.b("onZombieEvent FUNCTION END")
 end
